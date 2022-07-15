dc4dd7045287f690829367a6bb045428f93f0171
Jose Pavon-Alvarez <jose@breakingwave.com>
2022-07-15 13:35:30 +0100

    Withdraw tests

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -22,10 +22,27 @@ public class AccountTest {
     }
 
     @Test
-    public void depositMultipleAmountToIncreaseTheBalance() {
+    public void depositMultipleAmountsToIncreaseTheBalance() {
         Account account = new Account();
         account.deposit(100);
         account.deposit(50);
         assertThat(account.balance(), is(150));
+    }
+
+    @Test
+    public void withdrawAnAmountToDescreaseTheBalance() {
+        Account account = new Account();
+        account.deposit(200);
+        account.withdraw(100);
+        assertThat(account.balance(), is(100));
+    }
+
+    @Test
+    public void withdrawMultipleAmountsToDescreaseTheBalance() {
+        Account account = new Account();
+        account.deposit(200);
+        account.withdraw(100);
+        account.withdraw(50);
+        assertThat(account.balance(), is(50));
     }
 }
