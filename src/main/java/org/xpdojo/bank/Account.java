d14c9eefb5bd756f3010bf0b404abe1fb1f1caf4
Jose Pavon-Alvarez <jose@breakingwave.com>
2022-07-15 13:28:05 +0100

    Account class and first test!

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,5 +1,17 @@
 package org.xpdojo.bank;
 
 public class Account {
-    public int balance;
+    public int balance = 0;
+
+    public void initialize() {
+        balance = 0;
+    }
+
+    public int balance() {
+        return balance;
+    }
+    public int deposit(int amount) {
+        balance += amount;
+        return balance;
+    }
 }
